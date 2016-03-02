f07de8e970e64a07a2ff7e62a247a3a1d1f49d30
jdaruix <jamil_salim_daruix@hotmail.com>
2016-03-02 17:25:23 -0300

    .

--- app/src/main/java/br/com/exemplo/arrayadpter/MainActivity.java
+++ app/src/main/java/br/com/exemplo/arrayadpter/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.exemplo.arrayadpter;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.StringDef;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, countryArray);
+        ListView listView = (ListView) findViewById(R.id.country_list);
+        listView.setAdapter(adapter);
+
     }
 
     @Override
